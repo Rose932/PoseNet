8f249ebbb9e3292417efc6f38120b6845517327c
Stary <stary@Jaylias-MacBook-Pro.local>
2024-01-18 17:58:09 +0800

    final posenet

---
+++ precache-manifest.f61faf537b02b66b4a96febf7d8d35b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664c6d8b941b4f4d2bc360e3ebb908c3",
+    "url": "/PoseNet/index.html"
+  },
+  {
+    "revision": "d39582c325ba78935544",
+    "url": "/PoseNet/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "9ad096660435e2e94bb1",
+    "url": "/PoseNet/static/js/2.1ffeb580.chunk.js"
+  },
+  {
+    "revision": "056d2873e7c2868f4e8e8cad3dc44ef0",
+    "url": "/PoseNet/static/js/2.1ffeb580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39582c325ba78935544",
+    "url": "/PoseNet/static/js/main.c3f44cd9.chunk.js"
+  },
+  {
+    "revision": "ef3c4970149d12b9f52d",
+    "url": "/PoseNet/static/js/runtime-main.8c17c5ff.js"
+  }
+]);
